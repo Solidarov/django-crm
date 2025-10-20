50ac29ff040c9fa631bd7c329744148e23145247
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2025-10-20 16:32:14 +0200

    Add home page and routing
    	- Create new app 'core'
    	- Add base and index templates at 'templates/core'
    	- Create simple view of the home page
    	- Add routing to home page and connect to crm_core/urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Renders the main page "core/index.html"
+    """
+    return render(request, 'core/index.html')
