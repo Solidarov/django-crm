fce9e291242484bb6348349e22be96534143e5d0
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2025-11-07 09:10:12 +0100

    Remove: remove the unused imports

--- client/views.py
+++ client/views.py
@@ -1,11 +1,4 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.shortcuts import (
-    render,
-    get_object_or_404,
-    redirect,
-)
-from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
@@ -18,9 +11,6 @@ from django.views.generic import (
 
 from client.models import (
     Client,
-)
-from team.models import (
-    Plan,
 )
 from client.forms import (
     ClientForm,
